59ce5e4bf7ba1781c769e687ee62da760130020c
wizli595 <abdoowizli@gmail.com>
2023-12-30 07:15:17 +0100

    admin middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\admin\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,4 +29,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::resource('admin/dashboard', AdminController::class)->middleware(['auth', 'admin:admin'])->only('index');
